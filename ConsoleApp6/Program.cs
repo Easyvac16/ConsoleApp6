b1437d0fcf23a421387f13fb08081664913d3a69
Easyvac16 <gdima9051@gmail.com>
2023-10-11 20:26:10 +0300

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1 = int.Parse(args[0]);
+            int num2 = int.Parse(args[1]);
+            string operation = args[2];
+
+            int result = 0;
+            switch (operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+            }
+
+            Console.WriteLine($"Результат: {result}");
+
+
+        }
+
+
+    }
+
+
+}
